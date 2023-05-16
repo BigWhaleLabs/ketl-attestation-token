a5e7572e2673a75e975ae9164d1bced9072632fe
Michel Erokhov <mixaileroxov@gmail.com>
2023-05-16 15:18:43 +0200

    add lastId

--- contracts/KetlAttestation.sol
+++ contracts/KetlAttestation.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@big-whale-labs/versioned-contract/contracts/Versioned.sol";
 import "@opengsn/contracts/src/ERC2771Recipient.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
@@ -8,9 +9,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IAttestationCheckerVerifier.sol";
 
 contract KetlAttestation is ERC1155, Ownable, Versioned, ERC2771Recipient {
+  using Counters for Counters.Counter;
+
   uint public attestorPublicKey;
   IAttestationCheckerVerifier public attestationCheckerVerifier;
-  mapping(uint => uint) public attestationMerkleRoots;
+  Counters.Counter public lastId;
+  mapping(uint256 => uint) public attestationMerkleRoots;
   mapping(uint => bool) public nullifiers;
 
   constructor(
@@ -31,10 +35,18 @@ contract KetlAttestation is ERC1155, Ownable, Versioned, ERC2771Recipient {
     _setURI(_uri);
   }
 
-  function addAttestationMerkleRoot(
-    uint _id,
+  function addAttestationMerkleRoot(uint _merkleRoot) public onlyOwner {
+    uint256 _lastId = lastId.current();
+    attestationMerkleRoots[_lastId] = _merkleRoot;
+    lastId.increment();
+  }
+
+  function updateAttestationMerkleRoot(
+    uint256 _id,
     uint _merkleRoot
   ) public onlyOwner {
+    uint256 _lastId = lastId.current();
+    require(_id > _lastId, "Invalid id");
     attestationMerkleRoots[_id] = _merkleRoot;
   }
 
